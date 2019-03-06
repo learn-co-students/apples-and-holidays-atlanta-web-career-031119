7b588f3ce9cc3a4cce9b019752562f449e5d530c
Travis Martin <travisemerymartin@gmail.com>
2019-03-06 21:54:03 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
     cap_season=cap_season.split
     cap_season.map {|item| item=item.capitalize}
     season=cap_season.join(" ")
-    cap_day=day.to_s
+    cap_day=days.to_s
     cap_day=cap_day.split
     cap_day.map {|item| item=item.capitalize}
     day=cap_day.join(" ")
